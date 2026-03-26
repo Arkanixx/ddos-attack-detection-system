1253091574ae26b611e7001c0e06a4adac151252
Danilo Emil Bodden <danilobodden@gmail.com>
2026-03-26 02:43:16 -0400

    Added preprocessing pipeline and EDA notebook for DDoS dataset

--- eda.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df_raw = pd.read_csv('logs/Wednesday-21-02-2018_TrafficForML_CICFlowMeter_DDoS.csv')
-
-print(df_raw.head())
---
+++ preprocess.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+#loading the data without doing any preprocessing
+df_raw = pd.read_csv('logs/Wednesday-21-02-2018_TrafficForML_CICFlowMeter_DDoS.csv')
+
+print(df_raw.head())
+
+#---------------loading the data after doing the preprocessing-------------
+def clean_data(df):
+   
+   #clean column names
+   df.columns = df.columns.str.strip()
+
+   #replace any infinite values
+   df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+   #remove duplicates
+   df.drop_duplicates(inplace=True)
+
+   #Convert the timestamp column string to datetime
+   df["Timestamp"] = pd.to_datetime(df["Timestamp"], errors="coerce", dayfirst=True)
+
+   #handle any missing values
+   df.dropna(subset=["Label", "Timestamp"], inplace=True)
+
+   return df
+
+def encode_labels(df):
+   df["Label"] = df["Label"].apply(lambda x: 1 if x != "Benign" else 0 )
+   return df
+
+def save_clean_data(df, path):
+   df.to_csv(path, index=False)
+
+# ---------run preprocess pipeline--------------
+
+df_clean = clean_data(df_raw)
+df_clean = encode_labels(df_clean)
+
+# logging dataset changes
+print("Original rows:", df_raw.shape[0])
+print("Cleaned rows:", df_clean.shape[0])
+print("Rows removed:", df_raw.shape[0] - df_clean.shape[0])
+
+save_clean_data(df_clean, "logs/WednesdayTraffic_cleaned.csv")
+
+print("Cleaned dataset shape:", df_clean.shape)
+
+# print attack vs benign count
+print(df_clean["Label"].value_counts())
